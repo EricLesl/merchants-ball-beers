35ee5abf554a44b29e7680d3a35eb1c44f39a529
Eric Leslie <ericmacleanleslie@gmail.com>
2022-07-05 12:47:38 -0400

    woo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAZ5j_ogIZ7ehj3DtbFBaSSw329QS1YGys",
+    authDomain: "merchants-beers.firebaseapp.com",
+    databaseURL: "https://merchants-beers-default-rtdb.firebaseio.com",
+    projectId: "merchants-beers",
+    storageBucket: "merchants-beers.appspot.com",
+    messagingSenderId: "716223467721",
+    appId: "1:716223467721:web:4e42974925aa2ba4b27d18",
+    measurementId: "G-KBBTRDX6MN"
+  }
 };
